ae01290cd196eaf99122ed09e327c92e01ac6c8a
Drew Colthorp <dcolthorp@gmail.com>
2012-05-22 10:06:37 -0500

    absolute routes are routed from the topmost routable ancestor state of the current state

--- packages/ember-states/lib/router.js
+++ packages/ember-states/lib/router.js
@@ -2,12 +2,22 @@ require('ember-states/state');
 require('ember-states/route_matcher');
 require('ember-states/routable');
 
+var get = Ember.get;
+
 Ember.Router = Ember.StateManager.extend({
   route: function(path) {
-    if (path.charAt(0) === '/') {
+    var state = get(this, 'currentState');
+
+    if (path.charAt(0) === '/' || path.length == 0) {
       path = path.substr(1);
+
+      var parent;
+      while (get(state, 'route') && (parent = get(state, 'parentState'))) { 
+        state = parent; 
+      }
     }
 
-    this.send('routePath', path);
+
+    state.routePath(this, path);
   }
 });
